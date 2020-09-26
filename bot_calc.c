ca846e4cd6fd39e5466c9dcd1d768c5fa320b321
tiagompribeiro <tiagomiguelpr@gmail.com>
2020-09-26 18:22:35 +0100

    upload

--- bot_calc.c
+++ bot_calc.c
@@ -6,16 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc , char argv[]){
+int main (int argc , char *argv[]){
 
     if (argc<=1) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
-    //if (argv[1]<0) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
-    int arg = (int) strtol(argv[1], NULL, 10);
-    printf("%d sfve", arg);
-/*
-    for (int i=0;i<(int) argv[1];i++){
+    if (atoi(argv[1])<0) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
+
+
+    for (int i=0;i< atoi(argv[1]);i++){
         system("curl -d fnumber=4 localhost:80/cgi-bin/func.py");
     }
-*/
+
     return 0;
 }
