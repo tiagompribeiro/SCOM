56a1ba45fdae1aa2a8ed543c8e521d608839a70d
Rodrigo Caldas <48653110+rodzzyx@users.noreply.github.com>
2020-09-27 16:12:07 +0100

    Add files via upload

--- bot_calc.c
+++ bot_calc.c
@@ -5,16 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main (int argc , char *argv[]){
-
+    clock_t start,end;
     if (argc<=1) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
     if (atoi(argv[1])<0) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
 
-
+	
+	start=clock();
     for (int i=0;i< atoi(argv[1]);i++){
         system("curl -d fnumber=4 localhost:80/cgi-bin/func.py");
     }
-
+	end=clock();
+	double cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+	printf("took %f seconds to execute \n", cpu_time_used);
     return 0;
 }
