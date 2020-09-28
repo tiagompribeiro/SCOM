58bdb85d3b76a1149fa3a494271f2b8adbd056ec
tiagompribeiro <tiagomiguelpr@gmail.com>
2020-09-28 15:43:04 +0100

    upload

--- bot_calc.c
+++ bot_calc.c
@@ -13,28 +13,34 @@
 
 int main (int argc , char *argv[]){
     clock_t start,end;
-    if (argc<=1) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
-    if (atoi(argv[1])<0) {printf("usage: ./bot N , N=nº_calls (int>0)\n\n"); return 0;}
+    if (argc<=2) {printf("usage: ./bot N J, N=nº_calls  J=nº_tries (int>0)\n\n"); return 0;}
+    if (atoi(argv[1])<=0) {printf("usage: ./bot N J, N=nº_calls J=nº_tries (int>0)\n\n"); return 0;}
+    if (atoi(argv[2])<=0) {printf("usage: ./bot N J, N=nº_calls J=nº_tries  (int>0)\n\n"); return 0;}
+
     char str1[]="curl --silent -d fnumber=X localhost:80/cgi-bin/func.py > /dev/null";
 	double cpu_time_used_total= 0;
 	int random; 
     char aux;
+    FILE *f;
+    f = fopen("tempos.txt", "w");
 
-    for (int i=0;i< atoi(argv[1]);i++){
+    for (int j=0;j< atoi(argv[2]); j++){
+        for (int i=0;i< atoi(argv[1]);i++){
 
-        random = (rand() % (5 - 1 + 1)) + 1;
-        str1[25]= random + '0';
-        start=clock();
-        system(str1);
-        end=clock();
-        double cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
-        cpu_time_used_total+= cpu_time_used;
-        
-        
+            random = (rand() % (5 - 1 + 1)) + 1;
+            str1[25]= random + '0';
+            start=clock();
+            system(str1);
+            end=clock();
+            double cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+            cpu_time_used_total+= cpu_time_used;
+            
+            
+        }
+        printf("took %f seconds to execute \n", cpu_time_used_total);
+        fprintf(f,"%f \n", cpu_time_used_total);
+        cpu_time_used_total=0;
     }
-	
-	
-	printf("took %f seconds to execute \n", cpu_time_used_total);
     return 0;
 }
 
